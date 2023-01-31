aeacf71c53b7636ec52f55691bc4990a68242d86
Vyacheslav <nev-1@mail.ru>
2023-01-31 15:49:52 +0300

    сделал 1 задание 2 урока

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
-let title 
-let screens
-let screenPrice
-let rollback
-let fullPrice
-let adaptive
+let title = "Проект 1";
+let screens = "Простые, Сложные, Интерактивные";
+let screenPrice = 20;
+let rollback = Math.floor(Math.random() * 100);
+let fullPrice = 100000;
+let adaptive = true;
 
-alert("Hello!");
-console.log("Hello 2!");
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+
+console.log(screens.length);
+
+console.log("Стоимость верстки экранов " + screenPrice + " рублей");
+console.log("Стоимость разработки сайта " + fullPrice + " рублей");
+
+console.log(screens.toLocaleLowerCase().split());
+
+console.log(Math.floor(((fullPrice * (rollback/100)) / fullPrice) * 100) + " %");
