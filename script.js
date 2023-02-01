708168c2ca106bbb836110fd1e767bab0c58a424
Vyacheslav <nev-1@mail.ru>
2023-02-02 01:19:05 +0300

    добавил 3 урок

--- script.js
+++ script.js
@@ -17,3 +17,46 @@ console.log("Стоимость разработки сайта " + fullPrice + 
 console.log(screens.toLocaleLowerCase().split());
 
 console.log(Math.floor(((fullPrice * (rollback/100)) / fullPrice) * 100) + " %");
+
+// Урок 3 
+
+title = prompt('Как называется ваш проект?');
+screens = prompt('Какие типы экранов нужно разработать?');
+screenPrice = +prompt('Сколько будет стоить данная работа?');
+adaptive = prompt('Нужен ли адаптив на сайте?');
+
+if (adaptive == 'да' || adaptive == 'Да') {
+  adaptive = true;
+} else {
+  adaptive = false;
+}
+
+console.log(adaptive);
+
+let service1 = prompt('Какой дополнительный тип услуги нужен?');
+let servicePrice1 = +prompt('Сколько это будет стоить?');
+let service2 = prompt('Какой дополнительный тип услуги нужен?');
+let servicePrice2 = +prompt('Сколько это будет стоить?');
+
+fullPrice = (screenPrice + servicePrice1 + servicePrice2);
+
+
+let servicePercentPrice = (Math.ceil(fullPrice - rollback)); 
+console.log(servicePercentPrice);
+
+if ( fullPrice > 30000 ) 
+{
+  console.log("Даем скидку в 10%");
+} 
+else if ( fullPrice > 15000 || fullPrice < 30000 ) 
+{
+  console.log("Даем скидку в 5%");
+}
+else if ( fullPrice > 15000 || fullPrice > 0 )
+{
+  console.log("Скидка не предусмотрена");
+}
+else if ( fullPrice < 0 )
+{
+  console.log("Что то пошло не так");
+}
