4e8a495623a1d86425bd956668276539dc3e0141
Vyacheslav <nev-1@mail.ru>
2023-02-28 04:48:16 +0300

    внес изменения

--- script.js
+++ script.js
@@ -1,49 +1,25 @@
-let title = "Проект 1";
-let screens = "Простые, Сложные, Интерактивные";
-let screenPrice = 20;
-let rollback = Math.floor(Math.random() * 100);
-let fullPrice = 100000;
-let adaptive = true;
-
-console.log(typeof title);
-console.log(typeof fullPrice);
-console.log(typeof adaptive);
-
-console.log(screens.length);
-
-console.log("Стоимость верстки экранов " + screenPrice + " рублей");
-console.log("Стоимость разработки сайта " + fullPrice + " рублей");
-
-console.log(screens.toLocaleLowerCase().split());
-
-console.log(Math.floor(((fullPrice * (rollback/100)) / fullPrice) * 100) + " %");
-
-// Урок 3 
-
-title = prompt('Как называется ваш проект?');
-screens = prompt('Какие типы экранов нужно разработать?');
-screenPrice = +prompt('Сколько будет стоить данная работа?');
-adaptive = prompt('Нужен ли адаптив на сайте?');
-
-if (adaptive == 'да' || adaptive == 'Да') {
-  adaptive = true;
-} else {
-  adaptive = false;
-
-}
-
-console.log(adaptive);
-
+let title = prompt('Как называется ваш проект?');
+let screens = prompt('Какие типы экранов нужно разработать?');
+let screenPrice = +prompt('Сколько будет стоить данная работа?');
+let adaptive = confirm('Нужен ли адаптив на сайте?');
 let service1 = prompt('Какой дополнительный тип услуги нужен?');
 let servicePrice1 = +prompt('Сколько это будет стоить?');
 let service2 = prompt('Какой дополнительный тип услуги нужен?');
 let servicePrice2 = +prompt('Сколько это будет стоить?');
+let rollback = Math.floor(Math.random() * 100);
+let fullPrice = screenPrice + servicePrice1 + servicePrice2;
+let servicePercentPrice = fullPrice - (fullPrice * (rollback / 100)); 
 
-fullPrice = (screenPrice + servicePrice1 + servicePrice2);
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+console.log(screens.length);
+console.log("Стоимость верстки экранов " + screenPrice + " рублей");
+console.log("Стоимость разработки сайта " + fullPrice + " рублей");
+console.log(screens.toLocaleLowerCase().split());
+console.log(Math.floor(((fullPrice * (rollback/100)) / fullPrice) * 100) + " %");
 
-
-let servicePercentPrice = (Math.ceil(fullPrice - rollback)); 
-console.log(servicePercentPrice);
+// Урок 3 
 
 if ( fullPrice > 30000 ) 
 {
@@ -62,4 +38,10 @@ else if ( fullPrice < 0 )
   console.log("Что то пошло не так");
 }
 
-console.log();
+console.log(typeof title);
+console.log(typeof screenPrice);
+console.log(typeof adaptive);
+console.log(screens.length);
+console.log(servicePercentPrice);
+
+console.log("Стоимость верстки экранов " + screenPrice + " рублей" );
